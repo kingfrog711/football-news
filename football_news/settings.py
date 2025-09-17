13cc184079ceed1d46cbec49295decc53e70af25
kingfrog711 <gunata.sakri@gmail.com>
2025-09-17 14:11:10 +0700

    try to fix

--- football_news/settings.py
+++ football_news/settings.py
@@ -151,3 +151,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# to allow https connections on pws
+
+USE_X_FORWARDED_HOST = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
